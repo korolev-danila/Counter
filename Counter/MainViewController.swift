2a99b269b59728d6f28ec50ec4dd786379ef1c44
Danila <korolevdanila97@gmail.com>
2023-02-20 15:46:26 +0300

    create animation

--- Counter/MainViewController.swift
+++ Counter/MainViewController.swift
@@ -15,15 +15,29 @@ class MainViewController: UIViewController {
     
     private let disposeBag = DisposeBag()
     
+    private var changePlusButtonCenter: CGPoint!
+    private var goToTableButtonCenter: CGPoint!
+    private var zeroingButtonCenter: CGPoint!
+    private var buttonsHidden = false {
+        didSet {
+            UIView.transition(with: view, duration: 0.4, options: .transitionCrossDissolve) {
+                self.shuffleButton.isHidden = !self.buttonsHidden
+                self.changePlusButton.isHidden = self.buttonsHidden
+                self.goToTableButton.isHidden = self.buttonsHidden
+                self.zeroingButton.isHidden = self.buttonsHidden
+            }
+        }
+    }
+    
     private let digitsLabel: UILabel = {
         let label = UILabel()
-        label.text = ""
-        label.font = UIFont.systemFont(ofSize: 120)
+        label.text = "121212"
+        label.font = UIFont.systemFont(ofSize: 144)
         label.adjustsFontSizeToFitWidth = true
         label.numberOfLines = 1
-        label.minimumScaleFactor = 0.2
+        label.minimumScaleFactor = 0.02
         label.baselineAdjustment = .alignBaselines
-        label.textAlignment  = .center
+        label.textAlignment = .center
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -31,6 +45,16 @@ class MainViewController: UIViewController {
     private let tapButton: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let shuffleButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "minus.circle", withConfiguration:
+                                    UIImage.SymbolConfiguration(pointSize: 44, weight: .regular)), for: .normal) // minus.circle
+        button.contentMode = .scaleAspectFit
+        button.tintColor = .label
         return button
     }()
     
@@ -63,6 +87,7 @@ class MainViewController: UIViewController {
         return button
     }()
     
+    // MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
@@ -74,42 +99,67 @@ class MainViewController: UIViewController {
         
     }
     
+    // MARK: - Private method
     private func setupView() {
         view.backgroundColor = .systemBackground
         view.addSubview(digitsLabel)
         view.addSubview(tapButton)
+        
+        view.addSubview(shuffleButton)
         view.addSubview(changePlusButton)
         view.addSubview(goToTableButton)
         view.addSubview(zeroingButton)
-                
-        NSLayoutConstraint.activate([
-            digitsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            digitsLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
-        NSLayoutConstraint.activate([
-            tapButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            tapButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            tapButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            tapButton.heightAnchor.constraint(equalTo: view.widthAnchor, constant: 64)
-        ])
-        NSLayoutConstraint.activate([
-            changePlusButton.topAnchor.constraint(equalTo: goToTableButton.topAnchor),
-            changePlusButton.trailingAnchor.constraint(equalTo: goToTableButton.leadingAnchor, constant: -16),
-            changePlusButton.heightAnchor.constraint(equalToConstant: 44),
-            changePlusButton.widthAnchor.constraint(equalToConstant: 44)
-        ])
-        NSLayoutConstraint.activate([
-            goToTableButton.topAnchor.constraint(equalTo: tapButton.bottomAnchor, constant: 32),
-            goToTableButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            goToTableButton.heightAnchor.constraint(equalToConstant: 44),
-            goToTableButton.widthAnchor.constraint(equalToConstant: 44)
-        ])
-        NSLayoutConstraint.activate([
-            zeroingButton.topAnchor.constraint(equalTo: goToTableButton.topAnchor),
-            zeroingButton.leadingAnchor.constraint(equalTo: goToTableButton.trailingAnchor, constant: 16),
-            zeroingButton.heightAnchor.constraint(equalToConstant: 44),
-            zeroingButton.widthAnchor.constraint(equalToConstant: 44)
-        ])
+        
+        digitsLabel.frame = CGRect(x: 16, y: 0,
+                                   width: view.frame.size.width / 1.1,
+                                   height: view.frame.size.height / 1.1)
+        tapButton.frame = CGRect(x: 16, y: 128,
+                                 width: view.frame.size.width - 32,
+                                 height: view.frame.size.height / 1.5)
+        
+        shuffleButton.frame = CGRect(x: view.center.x - 32,
+                                     y: view.frame.size.height - 128,
+                                     width: 64, height: 64)
+        
+        changePlusButton.frame = CGRect(x: view.center.x - 96,
+                                     y: view.frame.size.height - 136,
+                                     width: 64, height: 64)
+        goToTableButton.frame = CGRect(x: view.center.x - 32,
+                                     y: view.frame.size.height - 136,
+                                     width: 64, height: 64)
+        zeroingButton.frame = CGRect(x: view.center.x + 32,
+                                     y: view.frame.size.height - 136,
+                                     width: 64, height: 64)
+
+        changePlusButtonCenter = changePlusButton.center
+        goToTableButtonCenter = goToTableButton.center
+        zeroingButtonCenter = zeroingButton.center
+        
+        changePlusButton.center = shuffleButton.center
+        goToTableButton.center = shuffleButton.center
+        zeroingButton.center = shuffleButton.center
+        
+        buttonsHidden = true
+    }
+    
+    private func animateButtons() {
+        if buttonsHidden {
+            UIView.animate(withDuration: 0.4) {
+                self.buttonsHidden = false
+                self.changePlusButton.center = self.changePlusButtonCenter
+                self.goToTableButton.center = self.goToTableButtonCenter
+                self.zeroingButton.center = self.zeroingButtonCenter
+            }
+        } else {
+            UIView.animate(withDuration: 0.4) {
+                self.changePlusButton.center = self.shuffleButton.center
+                self.goToTableButton.center = self.shuffleButton.center
+                self.zeroingButton.center = self.shuffleButton.center
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(40) ) {
+                self.buttonsHidden = true
+            }
+        }
     }
     
     private func setupBinding() {
@@ -121,6 +171,12 @@ class MainViewController: UIViewController {
             })
             .disposed(by: disposeBag)
         
+        shuffleButton.rx
+            .tap
+            .subscribe(onNext: { [weak self] in
+                self?.animateButtons()
+            })
+            .disposed(by: disposeBag)
         changePlusButton.rx
             .tap
             .subscribe(onNext: { [weak self] in
@@ -130,7 +186,7 @@ class MainViewController: UIViewController {
         goToTableButton.rx
             .tap
             .subscribe(onNext: { [weak self] in
-                self?.viewModel.updateCount()
+                self?.animateButtons()
             })
             .disposed(by: disposeBag)
         zeroingButton.rx
